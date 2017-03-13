a6866dc4c490032d88b1dede02f5107713ebc17c
Matt Roberts <mattjr72@gmail.com>
2017-03-13 10:06:14 +0000

    testing commit from eclipse

--- src/main/java/consumer/Application.java
+++ src/main/java/consumer/Application.java
@@ -31,7 +31,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author matt
  *
  */
-@SpringBootApplication
+@SpringBootApplication // TODO figure out what this annotation does
 public class Application {
 
   public static void main(String[] args) {
